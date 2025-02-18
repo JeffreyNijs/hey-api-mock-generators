6c95ae1990e3ab2be36441b56dd921421d1a116a
Jeffrey Nijs <109092992+JeffreyNijs@users.noreply.github.com>
2025-02-18 11:56:03 +0000

    Create npm-publish.yml

--- plugin.ts
+++ plugin.ts
@@ -43,16 +43,16 @@ export const handler: Plugin.Handler<any> = ({context, plugin}) => {
             path: plugin.output,
         });
 
-        let outputContent = 'import { generateMock } from "@hey-api/mock-generators";\n';
+        let outputContent = 'import { generateMock } from "hey-api-mock-generators";\n';
         outputContent += 'import type * as types from "./types.gen";\n\n';
 
         for (const [schemaName, schema] of Object.entries(schemas)) {
             if (!schema || typeof schema !== 'object') continue;
 
             const resolvedSchema = resolveRefs(schema, schemas);
-            const formattedSchemaName = schemaName.replace(/Schema$/, '');
-            const mockFunctionName = `${formattedSchemaName}Generator`;
-            const returnType = `types.${formattedSchemaName}`;
+            const typeName = schemaName.replace(/Schema$/, '')
+            const mockFunctionName = `generate${schemaName.replace(/Schema$/, '')}`;
+            const returnType = `types.${typeName}`;
 
             outputContent += `export function ${mockFunctionName}(): ${returnType} {
   return generateMock(${JSON.stringify(resolvedSchema, null, 2)}) as ${returnType};
